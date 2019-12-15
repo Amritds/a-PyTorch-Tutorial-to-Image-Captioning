8751c293fc2266eda73b2de759ba9b6931920f05
Amrit Seshadri <amritds18@gmail.com>
2019-12-15 03:18:36 -0800

    Framework for SCST algorithm created

--- train.py
+++ train.py
@@ -22,9 +22,8 @@ dropout = 0.5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
-# Training parameters
+# Training parameters (General)
 start_epoch = 0
-epochs = 120  # number of epochs to train for (if early stopping is not triggered)
 epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
 batch_size = 32
 workers = 1  # for data-loading; right now, only 1 works with h5py
@@ -37,13 +36,19 @@ print_freq = 100  # print training/validation stats every __ batches
 fine_tune_encoder = False  # fine-tune encoder?
 checkpoint = None  # path to checkpoint, None if none
 
+# Training parameters (Cross Entropy Maximization)
+epochs_XE = 120  # number of epochs to train for (if early stopping is not triggered)
+
+# Training parameters (Expected Reward Maximization)
+epochs_SCST = 120  # number of epochs to train for (if early stopping is not triggered)
+
 
 def main():
     """
     Training and validation.
     """
 
-    global best_bleu4, epochs_since_improvement, checkpoint, start_epoch, fine_tune_encoder, data_name, word_map
+    global best_bleu4, epochs_since_improvement, checkpoint, start_epoch, fine_tune_encoder, data_name, word_map, epochs
 
     # Read word map
     word_map_file = os.path.join(data_folder, 'WORDMAP_' + data_name + '.json')
@@ -82,9 +87,6 @@ def main():
     decoder = decoder.to(device)
     encoder = encoder.to(device)
 
-    # Loss function
-    criterion = nn.CrossEntropyLoss().to(device)
-
     # Custom dataloaders
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
@@ -95,8 +97,35 @@ def main():
         CaptionDataset(data_folder, data_name, 'VAL', transform=transforms.Compose([normalize])),
         batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
 
-    # Epochs
-    for epoch in range(start_epoch, epochs):
+    # Training to maximize cross entropy.
+    training_epochs(encoder, 
+    				decoder,
+    				encoder_optimizer,
+                    decoder_optimizer,
+                    train_loader,
+                    val_loader,
+                    device,
+                    train_type='XE')
+
+
+
+def training_epochs(encoder, decoder, encoder_optimizer, decoder_optimizer, train_loader, val_loader, device, train_type='XE'):
+	"""
+	Runs training epochs with checkpointing.
+	 :param train_type: Objective of:
+	                         (1)Maximizing Cross-Entropy
+	 				         (2)Maximizing Expected Reward using the SCST algorithm.
+	"""
+
+	global best_bleu4, epochs_since_improvement, checkpoint, start_epoch, fine_tune_encoder, data_name, word_map, epochs_XE, epochs_SCST
+
+    # Choose the correct number of epochs according to training type.
+    if(train_type=='XE'):
+        epochs = epochs_XE
+    else:
+        epochs = epochs_SCST
+
+	for epoch in range(start_epoch, epochs):
 
         # Decay learning rate if there is no improvement for 8 consecutive epochs, and terminate training after 20
         if epochs_since_improvement == 20:
@@ -106,14 +135,30 @@ def main():
             if fine_tune_encoder:
                 adjust_learning_rate(encoder_optimizer, 0.8)
 
-        # One epoch's training
-        train(train_loader=train_loader,
-              encoder=encoder,
-              decoder=decoder,
-              criterion=criterion,
-              encoder_optimizer=encoder_optimizer,
-              decoder_optimizer=decoder_optimizer,
-              epoch=epoch)
+        # One epoch's training -------------------------------
+        if(train_type=='XE'):
+
+            # Cross Entropy Loss function
+            criterion = nn.CrossEntropyLoss().to(device)
+            
+            train_XE(train_loader=train_loader,
+                     encoder=encoder,
+                     decoder=decoder,
+                     criterion=criterion,
+                     encoder_optimizer=encoder_optimizer,
+                     decoder_optimizer=decoder_optimizer,
+                     epoch=epoch)
+
+        elif(train_type=='SCST'):
+
+            train_SCST(train_loader=train_loader,
+                       encoder=encoder,
+                       decoder=decoder,
+                       criterion=criterion,
+                       encoder_optimizer=encoder_optimizer,
+                       decoder_optimizer=decoder_optimizer,
+                       epoch=epoch)
+        #-----------------------------------------------------
 
         # One epoch's validation
         recent_bleu4 = validate(val_loader=val_loader,
@@ -132,12 +177,12 @@ def main():
 
         # Save checkpoint
         save_checkpoint(data_name, epoch, epochs_since_improvement, encoder, decoder, encoder_optimizer,
-                        decoder_optimizer, recent_bleu4, is_best)
+                        decoder_optimizer, recent_bleu4, is_best)    
 
 
-def train(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_optimizer, epoch):
+def train_XE(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_optimizer, epoch):
     """
-    Performs one epoch's training.
+    Performs one epoch's training for Cross-Entropy maximization.
 
     :param train_loader: DataLoader for training data
     :param encoder: encoder model
@@ -220,6 +265,93 @@ def train(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_
                                                                           batch_time=batch_time,
                                                                           data_time=data_time, loss=losses,
                                                                           top5=top5accs))
+
+def train_SCST(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_optimizer, epoch):
+    """
+    Performs one epoch's training for Expected rewards maximization.
+
+    :param train_loader: DataLoader for training data
+    :param encoder: encoder model
+    :param decoder: decoder model
+    :param criterion: loss layer
+    :param encoder_optimizer: optimizer to update encoder's weights (if fine-tuning)
+    :param decoder_optimizer: optimizer to update decoder's weights
+    :param epoch: epoch number
+    """
+
+    decoder.train()  # train mode (dropout and batchnorm is used)
+    encoder.train()
+
+    batch_time = AverageMeter()  # forward prop. + back prop. time
+    data_time = AverageMeter()  # data loading time
+    losses = AverageMeter()  # loss (per word decoded)
+    top5accs = AverageMeter()  # top5 accuracy
+
+    start = time.time()
+
+    # Batches
+    for i, (imgs, caps, caplens) in enumerate(train_loader):
+        data_time.update(time.time() - start)
+
+        # Move to GPU, if available
+        imgs = imgs.to(device)
+        caps = caps.to(device)
+        caplens = caplens.to(device)
+
+        # Forward prop.
+        imgs = encoder(imgs)
+        scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(imgs, caps, caplens)
+
+        # Since we decoded starting with <start>, the targets are all words after <start>, up to <end>
+        targets = caps_sorted[:, 1:]
+
+        # Remove timesteps that we didn't decode at, or are pads
+        # pack_padded_sequence is an easy trick to do this
+        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
+
+        # Calculate loss
+        loss = criterion(scores, targets)
+
+        # Add doubly stochastic attention regularization
+        loss += alpha_c * ((1. - alphas.sum(dim=1)) ** 2).mean()
+
+        # Back prop.
+        decoder_optimizer.zero_grad()
+        if encoder_optimizer is not None:
+            encoder_optimizer.zero_grad()
+        loss.backward()
+
+        # Clip gradients
+        if grad_clip is not None:
+            clip_gradient(decoder_optimizer, grad_clip)
+            if encoder_optimizer is not None:
+                clip_gradient(encoder_optimizer, grad_clip)
+
+        # Update weights
+        decoder_optimizer.step()
+        if encoder_optimizer is not None:
+            encoder_optimizer.step()
+
+        # Keep track of metrics
+        top5 = accuracy(scores, targets, 5)
+        losses.update(loss.item(), sum(decode_lengths))
+        top5accs.update(top5, sum(decode_lengths))
+        batch_time.update(time.time() - start)
+
+        start = time.time()
+
+        # Print status
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data Load Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Top-5 Accuracy {top5.val:.3f} ({top5.avg:.3f})'.format(epoch, i, len(train_loader),
+                                                                          batch_time=batch_time,
+                                                                          data_time=data_time, loss=losses,
+                                                                          top5=top5accs))
+
 
 
 def validate(val_loader, encoder, decoder, criterion):
