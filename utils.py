b397ac5061f8c9befa3c211fa1044a8b0ab0e8af
Amrit Seshadri <amritds18@gmail.com>
2019-12-29 12:42:34 +0530

    Change to custom imports

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ from models import Encoder
 
 from nltk.translate.bleu_score import corpus_bleu
 
-from StackGAN.main_sampler import sample as image_generator
+from StackGAN.code.main_sampler import sample as image_generator
 
 # Data parameters
 data_folder = '/scratch/scratch5/adsue/caption_data'  # folder with data files saved by create_input_files.py
