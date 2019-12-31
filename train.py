4685d87502d6b9f5e528f7466e3a1010b6bfdd76
Amrit Seshadri <amritds18@gmail.com>
2019-12-31 13:13:46 +0530

    remove break constraints

--- train.py
+++ train.py
@@ -335,7 +335,7 @@ def train_XE(train_loader, encoder, decoder, criterion, encoder_optimizer, decod
                                                                           batch_time=batch_time,
                                                                           data_time=data_time, loss=losses,
                                                                           top5=top5accs))
-        break
+        
             
 def train_RL(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_optimizer, epoch):
     """
@@ -415,7 +415,7 @@ def train_RL(train_loader, encoder, decoder, criterion, encoder_optimizer, decod
                                                                           top5=top5accs))
 
 
-        break
+        
         
 def validate_XE(val_loader, encoder, decoder, criterion):
     """
@@ -510,7 +510,7 @@ def validate_XE(val_loader, encoder, decoder, criterion):
 
             assert len(references) == len(hypotheses)
 
-            break
+            
             
         # Calculate BLEU-4 scores
         bleu4 = corpus_bleu(references, hypotheses)
@@ -576,7 +576,7 @@ def validate_RL(val_loader, encoder, decoder, reward_function):
                       'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Minibatch Average Reward:{batch_avg_reward:.3f}')
 
-            break
+            
         avg_reward = sum_avg_rewards/counter
 
         print(
