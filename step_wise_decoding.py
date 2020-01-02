f692b5b87b2855549c77b67f70f8af79fdd8c6bc
Amrit Seshadri <amritds18@gmail.com>
2020-01-02 17:19:50 +0530

    correct typo

--- step_wise_decoding.py
+++ step_wise_decoding.py
@@ -74,7 +74,7 @@ def get_hypothesis_greedy(encoder_out, decoder, sample=False):
 
         # Add new words to sequences
         for (i, index) in enumerate(incomplete_inds):
-            seqs[index].append(next_words_inds[i])
+            seqs[index].append(next_word_inds[i])
         
        
         # sum scores of actions for incomplete sequences       
