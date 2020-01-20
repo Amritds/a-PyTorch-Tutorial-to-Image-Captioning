508474d377deef9a9fc6cc16b195d5834d2ad3a8
Amrit Seshadri <amritds18@gmail.com>
2020-01-20 14:08:53 +0530

    correct file paths.

--- encode_text.sh
+++ encode_text.sh
@@ -4,13 +4,14 @@
 # Extract text embeddings from the encoder
 #
 
-CUB_ENCODER=coco_gru18_bs64_cls0.5_ngf128_ndf128_a10_c512_80_net_T.t7 \
-CAPTION_PATH=/media/ssd/caption_data/mini_batch_captions \
+CUB_ENCODER=/scratch/scratch5/adsue/caption_data/coco_gru18_bs64_cls0.5_ngf128_ndf128_a10_c512_80_net_T.t7 \
+CAPTION_PATH=/scratch/scratch5/adsue/caption_data/mini_batch_captions.txt \
 GPU=1 \
 
 export CUDA_VISIBLE_DEVICES=${GPU}
 
-net_txt=/media/ssd/caption_data/text_encoder/${CUB_ENCODER} \
+net_txt=${CUB_ENCODER} \
 queries=${CAPTION_PATH}.txt \
 filenames=${CAPTION_PATH}.t7 \
+
 th ./get_embedding.lua
