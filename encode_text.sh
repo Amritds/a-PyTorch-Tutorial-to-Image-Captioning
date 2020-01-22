4e4774f0472a02f3f17a2ac4ffb12296f43b4b7e
Amritds <amrit.seshadri@ushur.com>
2020-01-22 14:07:44 +0530

    change file paths

--- encode_text.sh
+++ encode_text.sh
@@ -4,8 +4,8 @@
 # Extract text embeddings from the encoder
 #
 
-CUB_ENCODER=/scratch/scratch5/adsue/pretrained/coco_gru18_bs64_cls0.5_ngf128_ndf128_a10_c512_80_net_T.t7 \
-CAPTION_PATH=/scratch/scratch5/adsue/caption_data/mini_batch_captions.txt \
+CUB_ENCODER=/data2/adsue/pretrained/coco_gru18_bs64_cls0.5_ngf128_ndf128_a10_c512_80_net_T.t7 \
+CAPTION_PATH=/data2/adsue/caption_data/mini_batch_captions.txt \
 GPU=1 \
 
 export CUDA_VISIBLE_DEVICES=${GPU}
@@ -14,4 +14,4 @@ net_txt=${CUB_ENCODER} \
 queries=${CAPTION_PATH}.txt \
 filenames=${CAPTION_PATH}.t7 \
 
-th ./get_embedding.lua
+th /home/iitm/a-PyTorch-Tutorial-to-Image-Captioning/get_embedding.lua
