5ae6cd4ca8b66dc10f2d1d24dc8b3207def3d3bc
Amritds <amrit.seshadri@ushur.com>
2020-01-22 14:04:13 +0530

    change learning rate

--- train.py
+++ train.py
@@ -31,8 +31,8 @@ start_epoch = 0
 epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
 batch_size = 1
 workers = 0  # for data-loading; right now, only 1 works with h5py
-encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
-decoder_lr = 4e-4  # learning rate for decoder
+encoder_lr = 1e-5  # learning rate for encoder if fine-tuning
+decoder_lr = 4e-5  # learning rate for decoder
 grad_clip = 5.  # clip gradients at an absolute value of
 alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as in the paper
 
