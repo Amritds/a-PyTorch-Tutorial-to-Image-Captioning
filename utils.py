31f94cd8ab701d94ecf6e1322393b9f1facdbc9f
Amrit Seshadri <amritds18@gmail.com>
2020-03-23 07:30:19 -0700

    CIDErD optimization

--- utils.py
+++ utils.py
@@ -429,8 +429,8 @@ def cider_reward(imgs, hypothesis, save_imgs, ground_truth):
             train_sentence_index = json.load(f)
   
     # References
-    
-    ground_truth_sentence = [' '.join([rev_word_map[ind] for ind in sent if ind not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]) for sent.tolist() in ground_truth.tolist()]
+    ground_truth_list = [sent.tolist() for sent in ground_truth.tolist()]
+    ground_truth_sentence = [' '.join([rev_word_map[ind] for ind in sent if ind not in {word_map['<start>'], word_map['<end>'], word_map['<pad>']}]) for sent in ground_truth_list]
     
     
     reference_sentences = [train_sentence_index[true_sent] for (hyp,true_sent) in zip(hypothesis_sentences, ground_truth_sentence)]
